d86a39b89f699575966ee41a383ba57fb0d05a9c
sansrien <63850220+sansrien@users.noreply.github.com>
2022-02-28 14:29:35 +0800

    restructured

--- pre-processing/BatchProcess.sh
+++ pre-processing/BatchProcess.sh
@@ -3,5 +3,6 @@
 for i in {1...32...1} #for the 32 videos
 do
   bin/openposedemo.exe -video "examples/mediarun/$i.avi" --logging_level 0 --net_resolution "-320x160" --write_json "examples/output/$i" 
+  #bin/openposedemo.exe --image_dir "examples/lowlevel/" --net_resolution "-320x160" --write_images "output_images/"
   echo "$i" | python3 json2csv.py #json2csv.py should be in the openpose folder
 done
