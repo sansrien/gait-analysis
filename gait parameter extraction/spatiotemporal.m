da0ae5d697d3d47d9713f766422481bfa726882c
Samantha Joy Oguing <samantha.joy.oguing@eee.upd.edu.ph>
2022-03-05 23:10:29 +0800

    added temporal params (initial)

--- gait parameter extraction/spatiotemporal.m
+++ gait parameter extraction/spatiotemporal.m
@@ -23,27 +23,113 @@ tableLeftH.Properties.VariableNames(1:3) = {'Elapsed Time' 'Distance (x-coord)' 
 tableRightH = array2table(dataRightH);
 tableRightH.Properties.VariableNames(1:3) = {'Elapsed Time' 'Distance (x-coord)' 'Heel Location (y-coord)'};
 
+% Properties trial
+leftH.time = dataLeftH(:,1);
+leftH.yLoc = dataLeftH(:,2);
+leftH.xLoc = dataLeftH(:,3);
+rightH.time = dataRightH(:,1);
+rightH.yLoc = dataRightH(:,2);
+rightH.xLoc = dataRightH(:,3);
 
 %% Create function to get gait cycles
 % get peaks to get local minima and store in a vector
     % coordinate system: upper left = origin
     % y-coord: higher number, lower height
     % x-coord: higher number, farther distance from origin
-[pks, locs] = findpeaks(dataLeftH(:,3));
+[pksL, locsL] = findpeaks(dataLeftH(:,3));
+[pksR, locsR] = findpeaks(dataRightH(:,3));
 
 
 % create Mx2 matrix where M is the number of gait cycles while the columns 
 % indicate the two heel strike points for each gait cycle
-gaitCycles = zeros(length(pks)-1, 2);
-for i = 1:length(pks)
-    if i ~= length(pks)
-        gaitCycles(i,1) = pks(i);
-        gaitCycles(i,2) = pks(i+1);
+gaitCyclesL = zeros(length(pksL)-1, 2);
+for i = 1:length(pksL)
+    if i ~= length(pksL)
+        gaitCyclesL(i,1) = pksL(i);
+        gaitCyclesL(i,2) = pksL(i+1);
+    end
+end
+
+for i = 1:length(pksR)
+    if i ~= length(pksR)
+        gaitCyclesR(i,1) = pksR(i);
+        gaitCyclesR(i,2) = pksR(i+1);
     end
 end
 
 % create a variable for number of gait cycles
-numGaitCycle = length(gaitCycles);
+numGaitCycleL = length(gaitCyclesL);
+numGaitCycleR = length(gaitCyclesR);
+
+%% Calculation of Temporal Parameters
+
+% Get time associated with all heel strikes
+leftHeelTime = zeros(length(pksL)-1, 2);
+for i = 1:length(gaitCyclesL)
+    for j = 1:2
+        curr = gaitCyclesL(i,j);
+        currTimeIdx = find(dataLeftH(:,3) == curr);
+        leftHeelTime(i,j) = dataLeftH(currTimeIdx, 1); 
+    end
+end
+
+rightHeelTime = zeros(length(pksR)-1, 2);
+for i = 1:length(gaitCyclesR)
+    for j = 1:2
+        curr = gaitCyclesR(i,j);
+        currTimeIdx = find(dataRightH(:,3) == curr);
+        rightHeelTime(i,j) = dataRightH(currTimeIdx, 1); 
+    end
+end
+
+% Stride Time
+leftStrideTime = [];
+for i = 1:length(leftHeelTime(:,1))
+    leftStrideTime(i) = leftHeelTime(i,2) - leftHeelTime(i,1);
+end
+
+leftStrideTime = leftStrideTime.';
+avgStepTimeL = mean(leftStrideTime);
+
+rightStrideTime = [];
+for i = 1:length(rightHeelTime(:,1))
+    rightStrideTime(i) = rightHeelTime(i,2) - leftHeelTime(i,1);
+end
+
+rightStrideTime = rightStrideTime.';
+avgStepTimeR = mean(rightStrideTime);
+
+% Step Time //not correct pa ata?
+leftStepTime = [];
+for i = 1:length(rightHeelTime(:,1))
+    leftStepTime(i) = leftHeelTime(i,2) - rightHeelTime(i,1);
+end
+
+leftStepTime = leftStepTime.';
+avgStepTime = mean(leftStepTime);
+
+% Cadence
+cadence = (60/avgStepTimeL) + (60/avgStepTimeR);
+
+
+
+
+
+
+%% try lang kineme
+% create Mx2 matrix where M is the number of gait cycles while the columns 
+% indicate the two heel strike points for each gait cycle
+%gaitCycles2 = zeros(length(pks)-1, 2);
+% for i = 1:length(pks)
+%     if i ~= length(pks)
+%         test.time(i) = leftH.time(locs(i));
+%         test.xLoc(i) = leftH.xLoc(locs(i));
+%         test.yLoc(i) = leftH.yLoc(locs(i));
+%     end
+% end
+% 
+% % create a variable for number of gait cycles
+% numGaitCycle2 = length(test);
 
 
 
