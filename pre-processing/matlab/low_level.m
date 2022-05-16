a544aee314b782c214a71802041d589535df4295
sansrien <63850220+sansrien@users.noreply.github.com>
2022-05-16 13:40:36 +0800

    low level processing update

--- pre-processing/matlab/low_level.m
+++ pre-processing/matlab/low_level.m
@@ -1,12 +1,11 @@
 function vid_out = low_level(raw_video)
-    videoFileReader = VideoReader(raw_video);
-    vid_out = VideoWriter('vid_out.avi');
+    videoFileReader = VideoReader(raw_video + ".avi");
+    vid_out = VideoWriter(raw_video+'_processed.avi');
     open(vid_out);
     while hasFrame(videoFileReader)
         videoFrame = readFrame(videoFileReader);
         videoFrame_done = imlocalbrighten(videoFrame,'AlphaBlend',true);
         writeVideo(vid_out, videoFrame_done);
-        
     end
     close(vid_out);
 end
