b1dacfe4c0b388ac0ba750db257cd79586a4b5ba
Samantha Joy Oguing <samantha.joy.oguing@eee.upd.edu.ph>
2022-06-02 22:52:38 +0800

    updated mocap code (min dist, npeaks)

--- gait parameter extraction/mixnmatch.m
+++ gait parameter extraction/mixnmatch.m
@@ -1,5 +1,6 @@
 %% Read input file from pre-processing
-rgb_dataTable = readtable('p1s1_processed_switched.csv');
+rgb_dataTable = readtable('p17s1_processed_switched.csv');
+mocap_data = readtable('p17s1_mocap.csv');
 rgb_dataTable.Properties.VariableNames;
 
 %% Create time vector
@@ -89,7 +90,7 @@ end
 
 %% Mocap reading
 
-mocap_data = readtable('p7s1_mocap.csv');
+
 mocap_data.Properties.VariableNames;
 
 mocap_time = mocap_data.elapsed_time;
@@ -101,38 +102,42 @@ mocap_rheex = mocap_data.RHEE_y;
 mocap_rheey = mocap_data.RHEE_z;
 
 % Create vector for left and right heel
-LeftH = [timeVector lheex lheey];
-RightH = [timeVector rheex rheey];
+LeftH = [mocap_time mocap_lheex mocap_lheey];
+RightH = [mocap_time mocap_rheex mocap_rheey];
 
 lhee.time = LeftH(:,1);
 rhee.time = RightH(:,1);
 
-mocap_fc = 5;    %initially 10 from RRL but increased to 20 to detect the end heelstrikes
-mocap_Wn = mocap_fc/sampFreq;
+mocap_fc = mocap_fs/1.01;    %initially 10 from RRL but increased to 20 to detect the end heelstrikes
+mocap_Wn = mocap_fc/mocap_fs;
 [mB, mA] = butter(4,mocap_Wn,'low');
-lheey = filtfilt(mB,mA,lheey);
-rheey = filtfilt(mB,mA,rheey);
+lheey = filtfilt(mB,mA,mocap_lheey);
+rheey = filtfilt(mB,mA,mocap_rheey);
 
 [mocap_pksL mocap_locsL] = findpeaks(-lheey);
 [mocap_pksR mocap_locsR] = findpeaks(-rheey);
 
+mindist = length(lheey)/6;
+
 mocap_meanL = mean(mocap_pksL);
 mocap_threshL = 0.40*mocap_meanL; 
-[mocap_pksL, mocap_locsL] = findpeaks(-lheey, 'MinPeakDistance', sampFreq);
+[mocap_pksL, mocap_locsL] = findpeaks(-lheey, 'MinPeakDistance', mindist, 'NPeaks', 5);
 mocap_meanR = mean(mocap_pksR);
 mocap_threshR = 0.40*mocap_meanR;
-[mocap_pksR, mocap_locsR] = findpeaks(-rheey, 'MinPeakDistance', sampFreq);
+[mocap_pksR, mocap_locsR] = findpeaks(-rheey, 'MinPeakDistance', mindist, 'NPeaks', 6);
 
 if lhee.time(mocap_locsL(1)) > rhee.time(mocap_locsR(1)) && mocap_pksR(1) ~= 0 %left first
     mc_firstyholder = mocap_pksR(1);
-    mc_firstxholder = rheex(mocap_locsR(1));
+    mc_firstxholder = mocap_rheex(mocap_locsR(1));
     mocap_pksR(1) = [];
     mocap_locsR(1) = [];
+
 elseif lhee.time(mocap_locsL(1)) < rhee.time(mocap_locsR(1)) && mocap_pksL(1) ~= 0    %right first
     mc_firstyholder = mocap_pksL(1);
-    mc_firstxholder = lheex(mocap_locsL(1));
+    mc_firstxholder = mocap_lheex(mocap_locsL(1));
     mocap_pksL(1) = [];
     mocap_locsL(1) = [];
+
 else
     ;
 end
@@ -154,3 +159,102 @@ title('Filtered Right Leg Heel Location')
 legend('RGB', '', 'Mocap', '')
 hold off
 
+
+%% Calculation of Temporal Parameters
+% Get time associated with all heel strikes
+
+% leftHeelTime = zeros(length(mocap_locsL)-1, 2);
+% for i = 1:length(mocap_locsL)
+%     if i ~= length(mocap_locsL)
+%         leftHeelTime(i,1) = rgb_leftH.time(mocap_locsL(i));
+%         leftHeelTime(i,2) = rgb_leftH.time(mocap_locsL(i+1));
+%     end
+% end
+% 
+% rightHeelTime = zeros(length(mocap_locsR)-1, 2);
+% for i = 1:length(mocap_locsR)
+%     if i ~= length(mocap_locsR)
+%         rightHeelTime(i,1) = rgb_rightH.time(mocap_locsR(i));
+%         rightHeelTime(i,2) = rgb_rightH.time(mocap_locsR(i+1));
+%     end
+% end
+% 
+% 
+% %% Stride Time - OK
+% leftStrideTime = [];
+% for i = 1:length(leftHeelTime(:,1))
+%     leftStrideTime(i) = leftHeelTime(i,2) - leftHeelTime(i,1);
+% end
+% 
+% leftStrideTime = leftStrideTime.';
+% avgStrideTimeL = mean(leftStrideTime);
+% 
+% rightStrideTime = [];
+% for i = 1:length(rightHeelTime(:,1))
+%     rightStrideTime(i) = rightHeelTime(i,2) - rightHeelTime(i,1);
+% end
+% 
+% rightStrideTime = rightStrideTime.';
+% avgStrideTimeR = mean(rightStrideTime);
+% 
+% 
+% %% Step Time
+% if rightHeelTime(1) < leftHeelTime(1)   %for right first heel strikes
+%     leftStepTime = [];
+%     for i = 1:length(rightHeelTime(:,1))
+%         leftStepTime(i) = abs(leftHeelTime(i,1) - rightHeelTime(i,1));
+%     end
+% 
+%     leftStepTime = leftStepTime.';
+%     avgStepTimeL = mean(leftStepTime);
+% 
+% 
+%     rightStepTime = [];
+%     for i = 1:length(leftHeelTime(:,1))
+%         rightStepTime(i) = abs(rightHeelTime(i,2) - leftHeelTime(i,1));
+%     end
+% 
+%     rightStepTime = rightStepTime.';
+%     avgStepTimeR = mean(rightStepTime);
+% 
+% else %for left first heel strikes
+%     leftStepTime = [];
+%     for i = 1:length(rightHeelTime(:,1))
+%         leftStepTime(i) = abs(leftHeelTime(i,2) - rightHeelTime(i,1));
+%     end
+% 
+%     leftStepTime = leftStepTime.';
+%     avgStepTimeL = mean(leftStepTime);
+% 
+% 
+%     rightStepTime = [];
+%     for i = 1:length(leftHeelTime(:,1))
+%         rightStepTime(i) = abs(rightHeelTime(i,1) - leftHeelTime(i,1));
+%     end
+% 
+%     rightStepTime = rightStepTime.';
+%     avgStepTimeR = mean(rightStepTime); 
+% end
+% 
+% 
+% %% Cadence
+% cadence = (60/avgStrideTimeL) + (60/avgStrideTimeR);
+% 
+% 
+% %% Gait Speed
+% % avgStepLengthL = mean(leftStepLength);
+% % avgStepLengthR = mean(rightStepLength);
+% % 
+% % leftGaitSpeed = (avgStepLengthL/1000)/avgStepTimeL;
+% % rightGaitSpeed = (avgStepLengthR/1000)/avgStepTimeR;
+% 
+% 
+% % Create a table for display purposes
+% tempParams = [leftStrideTime rightStrideTime leftStepTime rightStepTime];
+% tableTempParams = array2table(tempParams);
+% tableTempParams.Properties.VariableNames(1:4) = {'Left Stride Time' 'Right Stride Time', 'Left Step Time', 'Right Step Time'};
+% avgTempParams = [avgStrideTimeL avgStrideTimeR avgStepTimeL avgStepTimeR cadence];
+% tableAvgTempParams = array2table(avgTempParams);
+% tableAvgTempParams.Properties.VariableNames(1:5) = {'Left Stride Time' 'Right Stride Time', 'Left Step Time', 'Right Step Time', 'Cadence'};
+% 
+% tableAvgTempParams
